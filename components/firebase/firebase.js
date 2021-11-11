6cba6da3cba520bc34495c6245aa095aac98d9d8
Gabrielle Soares <55507831+devgabrielle@users.noreply.github.com>
2021-11-11 19:23:21 -0300

    Create firebase.js

---
+++ components/firebase/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+//>>>> COLE SUA CHAVE AQUI <<<<
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
